f27074687d8f3ed3164db2e2c55605ce1806c749
Sean Champ <gimbal+github@metacommunity.info>
2014-09-23 11:56:38 -0700

    remove Garnet.ASDF specifier (legacy system definition)

--- garnet-loader.lisp
+++ garnet-loader.lisp
@@ -585,7 +585,6 @@ directory."
 (defvar Garnet-Protected-Eval-Pathname
   (append-directory Garnet-Binary-Pathname "protected-eval"))
 
-#-Garnet.ASDF
 (defvar Garnet-Bitmap-Pathname
   (append-directory Garnet-Lib-Pathname "bitmaps"))
 (defvar Garnet-Pixmap-Pathname
