8d7b01273508e004decdb1a19a641eefdb5fa130
Sean Champ <gimbal+github@metacommunity.info>
2014-09-23 16:06:59 -0700

    add more forms from garnet-loader.lisp

--- src/shared/shared.lisp
+++ src/shared/shared.lisp
@@ -9,6 +9,13 @@
 (pushnew :GARNET-V3.3 *features*)
 
 
+;; "The :GARNET-PROCESSES keyword goes on the *features* list if this version
+;; of lisp supports multiple processes.  Then things like the animation
+;; interactor can use the #+garnet-processes switch, instead of referring
+;; explicitly to different versions of lisp."
+;; -- garnet-loader.lisp
+(pushnew :GARNET-PROCESSES *features*)
+
 ;; "launch-process-p controls whether Garnet will launch
 ;;  a separate process to detect keyboard and mouse events."
 ;; -- garnet-loader.lisp
@@ -21,3 +28,24 @@
 (defvar update-locking-p T
   "If T, uses process locks to keep Update in a process from interrupting
    itself in a different process.")
+
+
+;; "The :GARNET-DEBUG option allows many different kinds of run-time checking,
+;; and also loads some extra test code.  After you have debugged your code
+;; and want it to run faster, remove :GARNET-DEBUG from the *features* list
+;; and RECOMPILE all of Garnet and your code.  The result will be smaller and
+;; somewhat faster.
+;; "To remove :GARNET-DEBUG from the *features* list, either defvar
+;; Garnet-Garnet-Debug to NIL before you load the garnet-loader, or simply
+;; comment out the next few lines."
+;; -- garnet-loader.lisp
+(defvar Garnet-Garnet-Debug T)
+(if Garnet-Garnet-Debug
+    (pushnew :garnet-debug *features*)
+    (setf *features* (delete :garnet-debug *features*)))
+
+
+#+allegro
+(defvar Garnet-Readtable *readtable*
+  "This variable is used by Allegro to restore the old value of the *readtable*
+when a saved image is restarted (see opal:make-image in opal/utils.lisp).")
