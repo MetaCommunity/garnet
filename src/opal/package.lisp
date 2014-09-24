c456c4bbcf02a88e90c8ce20687f7900b8d13b89
Sean Champ <gimbal+github@metacommunity.info>
2014-09-23 17:24:38 -0700

    add note about package definition location

--- src/opal/package.lisp
+++ src/opal/package.lisp
@@ -2,6 +2,10 @@
 
 (in-package #:cl-user)
 
+;; NOTE: Due to system dependencies, the Opal package is defined
+;; originally in ../gem/package.lisp
+;; similary in ../../garnet-loader.lisp
+
 (defpackage #:garnet-gadgets
   (:use #:common-lisp #:kr)
   (:nicknames #:gg))
