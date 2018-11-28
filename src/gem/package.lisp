0319acb73683cc10ac9e756a5c9b5b1625487b53
Sean Champ <spchamp@users.noreply.github.com>
2018-11-28 10:36:39 -0800

    Export more symbols for external access in this version of the Garnet system

--- src/gem/package.lisp
+++ src/gem/package.lisp
@@ -34,13 +34,149 @@
   ;; derived from garnet-loader.lisp
   (:use #:common-lisp
 	#:kr #:kr-debug)
-  (:export #:active-devices
-	   #:init-device))
+  (:export
+   #:active-devices
+   #:init-device
+
+   ;; referenced in gem x.lisp
+   #:*black*
+   #:set-draw-function-alist
+   #:set-window-property
+   #+clx #:x-set-window-property
+
+   ;; referenced in opal defs.lisp
+   ;;
+   ;; NB: Do these shadow anything?
+   #:display-info
+   #:make-display-info
+   #:copy-display-info
+   #:Display-Info
+   #:Make-Display-Info #:Copy-Display-Info
+   #:Display-Info-Display #:Display-Info-Screen #:Display-Info-Root-Window
+   #:Display-Info-Line-Style-GC #:Display-Info-Filling-Style-GC
+   #:*update-lock* #:*screen-width* #:*screen-height*
+   #:*Fixed-Font-Family* #:*Serif-Font-Family* #:*Sans-Serif-Font-Family*
+   #:*Small-Font-Size* #:*Medium-Font-Size*
+   #:*Large-Font-Size* #:*Very-Large-Font-Size*
+   #:*Small-Font-Point-Size* #:*Medium-Font-Point-Size*
+   #:*Large-Font-Point-Size* #:*Very-Large-Font-Point-Size*
+   #:default-font-from-file
+   ;; ... same file ...
+   #:*color-screen-p*
+
+   ;; referenced in opal basics.lisp
+   #:*function-alist*
+
+   ;; reference anywhere (may contain duplicates)
+   #:*exposure-event-mask
+   #:*Fixed-Font-Family*
+   #:*function-alist
+   #:*Large-Font-Point-Size*
+   #:*Large-Font-Size*
+   #:*Small-Font-Point-Size*
+   #:*Small-Font-Size*
+   #:*update-lock*
+   #:active-devices
+   #:all-garnet-windows
+   #:beep
+   #:bit-blit
+   #:black-white-pixel
+   #:character-width
+   #:Check-Double-Press
+   #:clear-area
+   #:clear-area.
+   #:color-to-index
+   #:colormap-property
+   #:copy-to-pixmap
+   #:create-cursor
+   #:create-image
+   #:create-image-array
+   #:create-pixmap
+   #:create-state-mask
+   #:create-window
+   #:default-font-from-file
+   #:delete-font
+   #:delete-pixmap
+   #:delete-window
+   #:device-image
+   #:Device-Image
+   #:discard-mouse-moved-events
+   #:discard-pending-events
+   #:Display-Info
+   #:DISPLAY-INFO
+   #:display-info-display
+   #:Display-Info-Display
+   #:display-info-filling-style-gc
+   #:display-info-line-style-gc
+   #:Display-Info-Line-Style-GC
+   #:draw-arc
+   #:draw-image
+   #:draw-line
+   #:draw-lines
+   #:draw-points
+   #:draw-rectangle
+   #:draw-roundtangle
+   #:draw-text
+   #:drawable-to-window
+   #:event-handler
+   #:flush-output
+   #:font-exists-p
+   #:font-max-min-width
+   #:font-name-p
+   #:font-to-internal
+   #:gdi-set-window-property
+   #:get-cut-buffer
+   #:image-bit
+   #:image-from-bits
+   #:image-hot-spot
+   #:image-size
+   #:image-to-array
+   #:init-device
+   #:initialize-device
+   #:initialize-window-borders
+   #:inject-event
+   #:MAC-set-window-property
+   #:Make-Display-Info
+   #:make-font-name
+   #:Make-Font-Name
+   #:map-and-wait
+   #:max-character-ascent
+   #:max-character-descent
+   #:mouse-grab
+   #:raise-or-lower
+   #:read-an-image
+   #:reparent
+   #:set-clip-mask
+   #:set-cut-buffer
+   #:set-device-variables
+   #:Set-Device-Variables
+   #:set-draw-function-alist
+   #:set-drawable-to-window
+   #:set-interest-in-moved
+   #:set-window-property
+   #:text-extents
+   #:Text-Extents
+   #:text-width
+   #:translate-character
+   #:translate-code
+   #:translate-coordinates
+   #:translate-mouse-character
+   #:window-debug-id
+   #:window-depth
+   #:window-from-drawable
+   #:window-has-grown
+   #:window-to-image
+   #:write-an-image
+   #:x-set-window-property
+
+  ))
 
 (defpackage #:opal
   ;; derived from garnet-loader.lisp
   (:use #:common-lisp
-	#:kr))
+	#:kr)
+  (:export #:black #:white ;; referfenced in x.lisp / Garnet 3.3 (??)
+           ))
 
 (defpackage #:interactors
   ;; derived from garnet-loader.lisp
